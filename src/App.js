5e22b3d09fe318db5895ae4b15269d64f31e09c4
amogh4319 <amogh4319@gmail.com>
2023-07-31 15:01:46 +0530

    adding summary to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import MainHeader from "./components/layout/MainHeader";
+import MealsSummary from "./components/layout/MealsSummary";
 function App() {
   return (
     <Fragment>
       <MainHeader/>
+      <MealsSummary/>
     </Fragment>
   );
 }
