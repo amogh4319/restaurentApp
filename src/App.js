c6c1bd59ba78b03b0d23f395896babcf3e56ea33
amogh4319 <amogh4319@gmail.com>
2023-07-31 13:58:16 +0530

    new restaurant app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './components/HeaderCartButton.module.css';
+import './components/MainHeader.css'
+function App() {
+  return (
+    <div className="header">
+      <h2>ReactMeals</h2>
+      <button className="button">Your Cart:0</button>
+    </div>
+  );
+}
+
+export default App;
